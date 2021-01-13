da815e14e840aeca7a72b9b97023fb56b270d89c
Sam Tholiya <tholiya.sam@outlook.in>
2021-01-14 03:59:01 +0530

    Connecting api with backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
+import { Button, Form, Input, message } from 'antd';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.dark.css';
+import { Analysis } from './Analysis';
+import { API_SERVER } from './config';
 
 function App() {
+
+  const [data, setData] = useState(undefined)
+  const [loading, setLoading] = useState(false)
+  const onFinish = (value) => {
+    setLoading(true)
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(value),
+    };
+    fetch(`${API_SERVER}/api/v1/analyser/html`, requestOptions)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(e => {message.error(e)})
+      .finally(()=>setLoading(false));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <body className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Form onFinish={onFinish}>
+          <Form.Item name="data" label="URL">
+            <Input placeholder="Enter a valid URL" />
+          </Form.Item>
+          <Button type="primary" htmlType="submit" style={{color:"white"}} type="submit">Analyse</Button>
+        </Form>
+        <Analysis loading={loading} data={data}/>
+      </body>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_SERVER = process.env.HOST_URL
